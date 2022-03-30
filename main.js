cb21a797848d21bfa51b1b47510d9f46870ec7fb
Rajveer <sidrathore663@gmail.com>
2022-03-31 00:26:47 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+document.getElementById( "title" ).style.color = "red";
+document.getElementById( "text" ).style.color = "blue";
+document.getElementById( "container" ).style.backgroundColor = "pink" ; 
+let div = document.createElement("div")
+let h1 = document.createElement("h1")
+let p = document.createElement("p")
+div.append(p)(h1)
